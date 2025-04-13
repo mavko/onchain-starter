2c0272be7df1daf7644778872ee8663ef5119003
Sebastian Selling <sebastian.selling@outlook.com>
2025-04-13 11:55:36 +0200

    removed react day picker

--- app/providers.tsx
+++ app/providers.tsx
@@ -1,21 +1,41 @@
-'use client';
+'use client'
 
-import { base } from 'wagmi/chains';
-import { OnchainKitProvider } from '@coinbase/onchainkit';
-import type { ReactNode } from 'react';
+import { OnchainKitProvider } from '@coinbase/onchainkit'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { WagmiProvider, createConfig, http } from 'wagmi'
+import { base, mainnet, bsc } from 'wagmi/chains'
+import {
+	coinbaseWallet,
+	injected,
+	metaMask,
+	walletConnect,
+} from 'wagmi/connectors'
 
-export function Providers(props: { children: ReactNode }) {
-  return (
-    <OnchainKitProvider
-      apiKey={process.env.NEXT_PUBLIC_ONCHAINKIT_API_KEY}
-          chain={base}
-          config={{ appearance: { 
-            mode: 'auto',
-        }
-      }}
-    >
-      {props.children}
-    </OnchainKitProvider>
-  );
+const config = createConfig({
+	chains: [mainnet, base, bsc],
+	connectors: [
+		injected({ target: 'binancewallet' }), // Binance Wallet
+		coinbaseWallet({ appName: 'Portfolio Tracker' }),
+		metaMask(),
+		walletConnect({
+			projectId: process.env.NEXT_PUBLIC_WALLETCONNECT_PROJECT_ID || '',
+		}),
+	],
+	transports: {
+		[mainnet.id]: http(),
+		[base.id]: http(),
+		[bsc.id]: http(),
+	},
+})
+
+const queryClient = new QueryClient()
+
+export function Providers({ children }: { children: React.ReactNode }) {
+	return (
+		<WagmiProvider config={config}>
+			<QueryClientProvider client={queryClient}>
+				<OnchainKitProvider>{children}</OnchainKitProvider>
+			</QueryClientProvider>
+		</WagmiProvider>
+	)
 }
-
